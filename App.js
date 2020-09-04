26b0737d1598162d47925d5d9b063989ff6bc096
Julian Urrego <julian.urrego.ar@gmail.com>
2020-09-04 17:16:03 -0400

    Flexbox class

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import Flexbox from "./Flexbox";
 
 export default function App() {
+  const [outputText, setOutputText] = useState("Open up App.js");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Flexbox />
+      {/* <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder='Course Goal'
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 2,
+            padding: 10,
+          }}
+        />
+        <Button title='Add' />
+      </View> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
